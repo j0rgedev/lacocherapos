a5bb7797ad7c0bf4c09cca8ef14780bba26cdb99
Jorge Antezana <102924003+jorgevfx@users.noreply.github.com>
2023-07-24 22:54:02 -0500

    :white_check_mark: Update: Main method to execute the whole application

--- src/main/java/LaCocheraPosApplication.java
+++ src/main/java/LaCocheraPosApplication.java
@@ -9,36 +9,12 @@ import view.pos.PointOfSaleFrame;
 
 public class LaCocheraPosApplication {
 
-        public static void main(String[] args) {
-            // Frame theme
-            FlatDarculaLaf.setup();
-//            testWholeApp();
-            testAdmin();
-//            testPos();
-        }
-
-        private static void testWholeApp(){
-            Login login = new Login();
-            LoginController loginController = new LoginController(login);
-            loginController.init();
-            login.setVisible(true);
-        }
-
-        private static void testPos(){
-            // POS view
-            PointOfSaleFrame pointOfSaleFrame = new PointOfSaleFrame();
-            // POS controller
-            PosFrameController posFrameController = new PosFrameController(pointOfSaleFrame);
-            posFrameController.init();
-            pointOfSaleFrame.setVisible(true);
-        }
-
-        private static void testAdmin(){
-            // Admin view
-            AdminIntranetFrame adminIntranetFrame = new AdminIntranetFrame();
-            // Admin controller
-            AdminFrameController adminFrameController = new AdminFrameController(adminIntranetFrame);
-            adminFrameController.init();
-            adminIntranetFrame.setVisible(true);
-        }
+    public static void main(String[] args) {
+        // Frame theme
+        FlatDarculaLaf.setup();
+        Login login = new Login();
+        LoginController loginController = new LoginController(login);
+        loginController.init();
+        login.setVisible(true);
+    }
 }
