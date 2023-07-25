e16d098dcba505c9f134e9a8ce1cd40e32976401
Jorge Antezana <102924003+jorgevfx@users.noreply.github.com>
2023-07-25 17:02:47 -0500

    :white_check_mark: Add: Logout button from POS

--- src/main/java/controller/pos/PosFrameController.java
+++ src/main/java/controller/pos/PosFrameController.java
@@ -1,5 +1,8 @@
 package controller.pos;
 
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import controller.login.LoginController;
+import view.login.Login;
 import view.pos.PointOfSaleFrame;
 
 import java.awt.*;
@@ -9,6 +12,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.swing.JOptionPane;
 
 public class PosFrameController extends AbstractController implements ActionListener {
 
@@ -40,15 +44,35 @@ public class PosFrameController extends AbstractController implements ActionList
 
     private void frameConfig() {
         pointOfSaleFrm.getContentPane().setBackground(Color.BLACK);
+        pointOfSaleFrm.posHeader.btnLogout.setIcon(new FlatSVGIcon("icons/logout.svg", 40, 40));
         // Initial panel: OrderPanel and CartPanel
         CartPanelController cartPanelController = new CartPanelController(pointOfSaleFrm);
         cartPanelController.init();
         OrderPanelController orderPanelController = new OrderPanelController(pointOfSaleFrm, cartPanelController);
         orderPanelController.init();
+
+        // Listeners
+        pointOfSaleFrm.posHeader.btnLogout.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
+        if(e.getSource() == pointOfSaleFrm.posHeader.btnLogout){
+            int dialogResult = JOptionPane.showConfirmDialog(
+                    null,
+                    "¿Está seguro que desea cerrar sesión?",
+                    "Cerrar sesión",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE
+            );
+            if(dialogResult == JOptionPane.YES_OPTION){
+                pointOfSaleFrm.dispose();
+                Login login = new Login();
+                LoginController loginController = new LoginController(login);
+                loginController.init();
+                login.setVisible(true);
+            }
+        }
     }
 
 }
--- src/main/java/view/components/pos/Header.java
+++ src/main/java/view/components/pos/Header.java
@@ -13,7 +13,8 @@ public class Header extends javax.swing.JPanel {
     private void initComponents() {
 
         lblTime = new javax.swing.JLabel();
-        txtSearchText = new TextFieldAnimation();
+        txtSearchText = new view.components.search.TextFieldAnimation();
+        btnLogout = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(45, 52, 63));
 
@@ -25,6 +26,11 @@ public class Header extends javax.swing.JPanel {
         txtSearchText.setForeground(new java.awt.Color(255, 255, 255));
         txtSearchText.setFont(new java.awt.Font("Iceland", 0, 24)); // NOI18N
 
+        btnLogout.setBackground(new java.awt.Color(45, 52, 63));
+        btnLogout.setBorderPainted(false);
+        btnLogout.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnLogout.setOpaque(true);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -32,8 +38,10 @@ public class Header extends javax.swing.JPanel {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(txtSearchText, javax.swing.GroupLayout.PREFERRED_SIZE, 380, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 214, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 191, Short.MAX_VALUE)
                 .addComponent(lblTime)
+                .addGap(20, 20, 20)
+                .addComponent(btnLogout, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(20, 20, 20))
         );
         layout.setVerticalGroup(
@@ -42,14 +50,16 @@ public class Header extends javax.swing.JPanel {
                 .addGap(16, 16, 16)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblTime, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtSearchText, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtSearchText, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnLogout, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(16, 16, 16))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton btnLogout;
     public javax.swing.JLabel lblTime;
-    public TextFieldAnimation txtSearchText;
+    public view.components.search.TextFieldAnimation txtSearchText;
     // End of variables declaration//GEN-END:variables
 }
